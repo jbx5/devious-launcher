d002666f70a12ecc827c6c24160e7e9e91572f63
jbx5 <113217895+jbx5@users.noreply.github.com>
2022-09-16 11:43:18 -0700

    update resources

--- src/main/java/net/runelite/launcher/ColorScheme.java
+++ src/main/java/net/runelite/launcher/ColorScheme.java
@@ -32,10 +32,10 @@ import java.awt.Color;
 class ColorScheme
 {
 	/* The blue color used for the branding's accents */
-	public static final Color BRAND_BLUE = new Color(130, 15, 46);
+	public static final Color BRAND_BLUE = new Color(50, 160, 250);
 
 	/* The blue color used for the branding's accents, with lowered opacity */
-	public static final Color BRAND_BLUE_TRANSPARENT = new Color(130, 15, 46, 120);
+	public static final Color BRAND_BLUE_TRANSPARENT = new Color(50, 160, 250, 120);
 
 
 	static final Color DARK_GRAY_COLOR = new Color(40, 40, 40);
